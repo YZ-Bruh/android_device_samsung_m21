24752d3d04b4e2434e1999e929625db176a68cb6
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-08-14 18:18:17 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_m21-user
-add_lunch_combo omni_m21-userdebug
-add_lunch_combo omni_m21-eng
+add_lunch_combo twrp_m21-user
+add_lunch_combo twrp_m21-userdebug
+add_lunch_combo twrp_m21-eng
